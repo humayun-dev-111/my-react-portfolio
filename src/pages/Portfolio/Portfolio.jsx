131a2fead82865e0e35e6ab580a4fb671affbc08
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-06-03 12:16:07 +0600

    projects modify

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,95 +1,89 @@
 import { FaExternalLinkSquareAlt } from "react-icons/fa";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 AOS.init();
 
 const Portfolio = () => {
-    return (
-        <div className="container mx-auto px-5 pt-32 pb-20">
-            <div className="pt-10 flex gap-20 text-white flex-col">
-                <div className="flex gap-12 flex-col lg:flex-row">
-                    <div data-aos-duration="1000" data-aos="fade-up" className='w-full lg:w-[30%] overflow-hidden rounded-xl'>
-                        <img className="object-cover rounded-xl w-full hover:scale-105 duration-200" src="https://i.postimg.cc/vHvZVsS6/medcamp.jpg" alt="Medcamp Image" />
-                    </div>
-                    <div data-aos-duration="1000" data-aos="fade-up" className='w-full lg:w-[70%]'>
-                        <div className='flex items-center gap-4 flex-col md:flex-row'>
-                            <div className="flex items-center gap-5">
-                                <h2 className="text-2xl text-center md:text-left font-semibold">Medcamp</h2>
-                                <p className='text-lg text-center md:text-left font-medium text-nowrap pt-1'>(Medical Camp Website)</p>
-                            </div>
-                            <div className='flex text-blue-200 gap-7 ml-7'>
-                                <a
-                                    href="https://medcamp.netlify.app" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Live Site <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                                <a href="https://github.com/programming-hero-web-course1/b9a12-client-side-mdrieadmia.git" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Client Code <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                                <a href="https://github.com/programming-hero-web-course1/b9a12-server-side-mdrieadmia.git" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Server Code <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                            </div>
-                        </div>
-                        <p className='text-justify pt-5'>This camp management system features a dashboard for organizers to manage camps and participants, with CRUD operations to create, update, and delete camps. Participants can manage their camp participation and pay fees via the integrated Stripe payment gateway.The project utilizes React for the front-end, ExpressJS and NodeJS for the back-end, and MongoDB for the database. Firebase handles authentication, while Tailwind CSS ensures a responsive UI. JavaScript provides core functionality, and Recharts offers data visualization with insightful charts. This system enhances the experience for organizers and participants with its user-friendly interface and secure payment options.</p>
-                        <p className='pt-2 font-semibold' >Technology Used : React, ExpressJS, NodeJS, MongoDB, Firebase, Tailwind, JavaScript, Stripe, Recharts.</p>
-                    </div>
-                </div>
-                <div className="flex gap-12 flex-col lg:flex-row">
-                    <div data-aos-duration="1000" data-aos="fade-up" className='w-full lg:w-[30%] overflow-hidden rounded-xl'>
-                        <img className="object-cover rounded-xl w-full hover:scale-105 duration-200" src="https://i.postimg.cc/ZR3TKtdn/scribble.jpg" alt="Scribble Image" />
-                    </div>
-                    <div data-aos-duration="1000" data-aos="fade-up" className='w-full lg:w-[70%]'>
-                        <div className='flex items-center flex-col md:flex-row gap-4'>
-                            <div className="flex items-center gap-5">
-                                <h2 className="text-2xl text-center md:text-left font-semibold">Scribble</h2>
-                                <p className='text-lg text-center md:text-left font-medium text-nowrap pt-1'>(Blog Website)</p>
-                            </div>
-                            <div className='flex text-blue-200 gap-7 ml-10'>
-                                <a href="https://scribblebd.netlify.app" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Live Site <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                                <a href="https://github.com/Porgramming-Hero-web-course/b9a11-client-side-mdrieadmia.git" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Client Code <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                                <a href="https://github.com/Porgramming-Hero-web-course/b9a11-server-side-mdrieadmia.git" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Server Code <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                            </div>
-                        </div>
-                        <p className='text-justify pt-5'>Implemented user authentication to secure the website and manage user access effectively. Developed comprehensive CRUD operations, enabling users to create, read, update, and delete data entries seamlessly. Ensured the website features a fully responsive design, providing an optimal viewing experience across various devices and screen sizes. This approach not only enhances security but also improves functionality and user experience, making the platform robust and user-friendly. Through these features, the website delivers a secure, efficient, and accessible interface for all users, ensuring smooth operations and a satisfying user experience on any device.</p>
-                        <p className='pt-2 font-semibold' >Technology Used : React, NodeJS, ExpressJS, MongoDB, JavaScript, Firebase, Tailwind, Flowbite.</p>
-                    </div>
-                </div>
-                <div className="flex gap-12 flex-col lg:flex-row">
-                    <div data-aos-duration="1000" data-aos="fade-up" className='w-full lg:w-[30%] overflow-hidden rounded-xl'>
-                        <img className="object-cover rounded-xl w-full hover:scale-105 duration-200" src="https://i.postimg.cc/RFYQzYHg/travello.jpg" alt="Travello Image" />
-                    </div>
-                    <div data-aos-duration="1000" data-aos="fade-up" className='w-full lg:w-[70%]'>
-                        <div className='flex items-center flex-col md:flex-row gap-4'>
-                            <div className="flex items-center gap-5">
-                                <h2 className="text-2xl text-center md:text-left font-semibold">Travello</h2>
-                                <p className='text-lg text-center md:text-left font-medium text-nowrap pt-1'>(Tours & Travel Website)</p>
-                            </div>
-                            <div className='flex text-blue-200 gap-7 ml-10'>
-                                <a href="https://scribblebd.netlify.app" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Live Site <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                                <a href="https://github.com/Porgramming-Hero-web-course/b9a11-client-side-mdrieadmia.git" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Client Code <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                                <a href="https://github.com/Porgramming-Hero-web-course/b9a11-server-side-mdrieadmia.git" target="_blank">
-                                    <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">Server Code <FaExternalLinkSquareAlt className="mb-[2px] ml-2" /></p>
-                                </a>
-                            </div>
-                        </div>
-                        <p className='text-justify pt-5'>Implemented user authentication to secure the website and manage user access effectively. Developed comprehensive CRUD operations, enabling users to create, read, update, and delete data entries seamlessly. Ensured the website features a fully responsive design, providing an optimal viewing experience across various devices and screen sizes. This approach not only enhances security but also improves functionality and user experience, making the platform robust and user-friendly. Through these features, the website delivers a secure, efficient, and accessible interface for all users, ensuring smooth operations and a satisfying user experience on any device.</p>
-                        <p className='pt-2 font-semibold' >Technology Used : React, NodeJS, ExpressJS, MongoDB, JavaScript, Firebase, Tailwind, Flowbite.</p>
-                    </div>
-                </div>
-                
+  return (
+    <div className="container mx-auto px-5 pt-32 pb-20">
+      <div className="pt-10 flex gap-20 text-white flex-col">
+        <div className="flex gap-12 flex-col lg:flex-row">
+          <div
+            data-aos-duration="1000"
+            data-aos="fade-up"
+            className="w-full lg:w-[30%] overflow-hidden rounded-xl"
+          >
+            <img
+              className="rounded-xl w-full h-[220px] md:h-[280px] lg:h-[240px] object-cover object-top hover:scale-105 duration-200"
+              src="/ScneAds.png"
+              alt="Scne Ads Image"
+            />
+          </div>
+          <div
+            data-aos-duration="1000"
+            data-aos="fade-up"
+            className="w-full lg:w-[70%]"
+          >
+            <div className="flex items-center gap-4 flex-col md:flex-row">
+              <div className="flex items-center gap-5">
+                <h2 className="text-2xl text-center md:text-left font-semibold">
+                  Scne Ads
+                </h2>
+                <p className="text-lg text-center md:text-left font-medium text-nowrap pt-1">
+                  (BillBoard Booking Website)
+                </p>
+              </div>
+              <div className="flex text-blue-200 gap-7 ml-7">
+                <a href="https://scne-ads.netlify.app" target="_blank">
+                  <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">
+                    Live Site{" "}
+                    <FaExternalLinkSquareAlt className="mb-[2px] ml-2" />
+                  </p>
+                </a>
+                <a
+                  href="https://github.com/humayun-dev-111/Scne-Ads-Frontend"
+                  target="_blank"
+                >
+                  <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">
+                    Client Code{" "}
+                    <FaExternalLinkSquareAlt className="mb-[2px] ml-2" />
+                  </p>
+                </a>
+                <a
+                  href="https://github.com/humayun-dev-111/Scne-Ads-Backend"
+                  target="_blank"
+                >
+                  <p className="inline-flex  items-center hover:text-blue-600 duration-200 text-nowrap">
+                    Server Code{" "}
+                    <FaExternalLinkSquareAlt className="mb-[2px] ml-2" />
+                  </p>
+                </a>
+              </div>
             </div>
+            <p className="text-justify pt-5">
+              Developed a full-stack ads management and e-commerce platform that
+              connects businesses with billboard owners, enabling seamless
+              advertisement creation, management, and display. The platform
+              features an interactive Google Maps integration for discovering
+              available billboard locations, secure Stripe payment processing
+              for ad campaigns, a centralized admin dashboard for managing
+              users, screens, and campaigns, and a real-time WebSocket-based
+              chat system that facilitates instant communication between
+              businesses and billboard owners while providing live campaign
+              updates.
+            </p>
+            <p className="pt-2 font-semibold">
+              Technology Used : React, TypeScript, Redux, Tailwind CSS,
+              ShadCN/UI, Google Maps API, LightswindUI, Node.js, Express.js,
+              PostgreSQL, Prisma ORM, Supabase , Stripe( Payment ), WebSocket,
+              node-cron .
+            </p>
+          </div>
         </div>
-    );
+        
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
